ef81b289d987d21df7ca4ef4dbaf290e09fbe7af
shubhpraaa <shubhra.prakash2023@vitstudent.ac.in>
2025-05-25 01:53:05 +0530

    fixed unknown import

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
 import Header from './components/Header/Header'
-import Footer from './components/Footer/Footer'
 
 function Layout() {
   return (
